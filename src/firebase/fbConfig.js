4e480801b26c180fb993406f3ffc8426487055a6
Sahil Kumar <54372288+sahillkumar@users.noreply.github.com>
2022-05-26 12:54:33 +0530

    Update fbConfig.js
    remove firebase config

--- src/firebase/fbConfig.js
+++ src/firebase/fbConfig.js
@@ -3,12 +3,12 @@ import "firebase/firestore"
 import 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBgmUq0E2ECDiu-cb63-s_drHCgH3He1XY",
-    authDomain: "mario-plan-e3ae0.firebaseapp.com",
-    projectId: "mario-plan-e3ae0",
-    storageBucket: "mario-plan-e3ae0.appspot.com",
-    messagingSenderId: "445403269475",
-    appId: "1:445403269475:web:994cc00dc9682b77a36b56"
+    apiKey: "apikey",
+    authDomain: "authdomain",
+    projectId: "projectId",
+    storageBucket: "storagebucket",
+    messagingSenderId: "senderID",
+    appId: "appId"
   };
 
 firebase.initializeApp(firebaseConfig)
